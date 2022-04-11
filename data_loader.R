f59c89b9396f6769260f5bd7df1556df52c6b65b
Daniel-Philip Saggau <daniel.saggau@campus.lmu.de>
2022-04-11 12:19:47 +0200

    files

--- data_loader.R
+++ data_loader.R
@@ -1,14 +1,8 @@
 # dataloader
+# reference: https://mlr3oml.mlr-org.com/reference/OMLData.html
 
-odata = OMLData$new(id = 41144)
-
-print(odata)
-print(odata$target_names)
-print(odata$feature_names)
-
-mlr3oml::read_arff("https:/old.openml.org/data/v1/download/19335517/madeline.arff")
-library(mlr3)
-tsk("oml", data_id = 41144)
 oml_data = OMLData$new(41144)
-oml_data$name
-data = oml_data$data
+madeline = oml_data$data
+
+oml_data = OMLData$new(1485)
+madelon = oml_data$data
