9da0c4b662cbd56c2734859ea08f8659fc297f7a
Daniel-Philip Saggau <daniel.saggau@campus.lmu.de>
2022-04-17 15:33:12 +0200

    added code

--- R/automl.R
+++ R/automl.R
@@ -1,14 +1,65 @@
 # auto ml tool
-library(mlr3tuning)
+#@required: library(mlr3tuning)
 
-automl = function(task, learner = NULL, measure = NULL, runtime = NULL, terminator = NULL )){
-measure = msrs(c("classif.ce"))
-#learners = makeLearner("classif.xgboost", eval_metric ="")
-learner = lrn("classif.xgboost"),
-terminator =  trm("none"),
-filter flt(),
-tuner = tnr("grid_search"),
-resampling = rsmp("cv", folds = 10)
+#@ input param: terminator
+#@ input param: task
+#@ input param: learner
+#@ input param: filter
+#@ output:
+
+automl = function(task, learner = NULL, filter = NULL ,measure = NULL, terminator = NULL){
+benchmarkgrid( # unsure whether combinable
+
+task = tsks(task))
+
+learner = lrn("classif.xgboost", # based on default values
+              eta = to_tune(lower = 1e-04, upper =1 , logscale = TRUE),
+              nrounds = to_tune(lower = 1e+00, upper = 5000, logscale= FALSE),
+              max_depth = to_tune(lower = 1e+00, upper 20= , logscale =FALSE),
+              colsample_bytree = to_tune(lower = 1e-01, upper=1, logscale= FALSE),
+              colsample_bylevel = to_tune(lower = 1e-01, upper= 1, logscale = FALSE),
+              lambda = to_tune(lower = 1e-03 , upper = 1000, logscale = TRUE),
+              alpha = to_tune(lower = 1e-03, upper = 1000, logscale = TRUE),
+              subsample= to_tune(lower = 1e-01, upper = 1, logscale = FALSE)
+)
+
+measures = msrs(measures)
+
+terminator =  terminator
+
+filter = flt(filter)
+
+tuner = tnr("grid_search", batch_size =10)
+
+resampling = rsmps("cv", folds = 10)
+
+instance = tune(
+  method = tuner,
+  task = task,
+  leaarner = learner,
+  resampling = resampling,
+  measure = measures,
+  term_evals = 50
+)
+
+
+instance$results
+
+learner$param_set$values = instance$result_learner_param_vals
+
+#learner$train(task) make sense here ?
+
+
+
+
+
 }
 
+automl(
+task = c(madeline,madelon),
+learner = "xgboost",
+measures = c("classif_auc","time_train"),
+terminator = evals20,
+filter = "auc"
+)
 
